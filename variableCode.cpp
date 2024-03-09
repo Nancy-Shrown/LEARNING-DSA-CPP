2a9f1d791ae369d43e47899bc29a653735e4eea9
Nancy-Shrown <nancyshrown@gmail.com>
2024-03-09 10:25:02 +0530

    first commit

---
+++ variableCode.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cout<<"Enter your name: ";
+    cin>>name;
+    cout<<"Your name is "<<name<<endl;
+
+    int a,b;
+    cout<<"a:";
+    cin>>a;
+    cout<<"b";
+    cin>>b;
+    int c;
+    
+    return 0;
+}
