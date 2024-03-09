988cb5397e5e954cfcf277351cc462afa96b4395
Nancy-Shrown <nancyshrown@gmail.com>
2024-03-09 12:22:40 +0530

    var

--- variableCode.cpp
+++ variableCode.cpp
@@ -5,14 +5,19 @@ int main()
     string name;
     cout<<"Enter your name: ";
     cin>>name;
-    cout<<"Your name is "<<name<<endl;
+    cout<<"Your name is "<< name<<endl;
 
+    //swap two variables//
     int a,b;
     cout<<"a:";
     cin>>a;
     cout<<"b";
     cin>>b;
     int c;
-    
+    c=b;
+    b=a;
+    a=c;
+    cout<<"Value of a : "<< a<<endl;
+    cout<<"Value of b : "<< b<<endl; 
     return 0;
 }
