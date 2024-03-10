e8ae01eb411878002029fb9ae058cc5b2bfb295b
Nancy-Shrown <nancyshrown@gmail.com>
2024-03-10 16:51:50 +0530

    if-esle switch

--- BASIC OF C++/ifelseSwtich.cpp
+++ BASIC OF C++/ifelseSwtich.cpp
@@ -3,13 +3,54 @@ using namespace std;
 
 int main()
 {
+
     int age;
     cout<<"Enter age:";
     cin>> age;
     if(age<12)
-    cout<<"child";
-    elseif(12<age<18)
-    cout<<"Teenager";
-    else(age>18)
-    cout<<"Adult";
+    {
+        cout<<"child"<<endl;
+    }
+    else if(age>18)
+    {
+        cout<<"Adult"<<endl;
+    }
+    else
+    {
+        cout<<"Teenager"<<endl;
+    }
+
+    //switch statement
+    int num;
+    cout<<"Select cousre number:";
+    cin>>num;
+    switch(num)
+    {
+        case 1:
+        {
+            cout<<"C++";
+            break;
+        }
+        case 2:
+        {
+            cout<<"Java";
+            break;
+        }
+        case 3:
+        {
+            cout<<"Python";
+            break;
+        }
+        default :
+        {
+            cout<<"Web Development";
+        }
+
+    }
+    
+
+
+
+    return 0;
+
 }
