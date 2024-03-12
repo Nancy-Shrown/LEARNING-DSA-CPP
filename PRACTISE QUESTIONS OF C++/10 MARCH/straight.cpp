e91ddb490d6828750547ffb69378e9fe6ad16d17
Nancy-Shrown <nancyshrown@gmail.com>
2024-03-12 20:22:04 +0530

    complete

---
+++ PRACTISE QUESTIONS OF C++/10 MARCH/straight.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x1,y1,x2,y2;
+    cout<<"Enter value of x coodinates : ";
+    cin>>x1>>x2;
+
+    cout<<"Enter value of y coodinates : ";
+    cin>>y1>>y2;
+
+
+    
+    return 0;
+}
