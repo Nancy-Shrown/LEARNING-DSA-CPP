1a1c021a98a454d1c000a62476a468bdd1be6075
Nancy-Shrown <nancyshrown@gmail.com>
2024-03-12 20:37:32 +0530

    complete

---
+++ PRACTISE QUESTIONS OF C++/12  MARCH/tableof19.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,i,table;
+    n= 19;
+    for (i=1;i<=10;i++)
+    {
+        table = n*i;
+        cout<<n<<" * "<<i<<" = "<<table<<endl;
+    }
+}
