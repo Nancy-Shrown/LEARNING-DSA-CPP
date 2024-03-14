d38d899ccdb5e09105c2a084bd23e0614e4c3303
Nancy-Shrown <nancyshrown@gmail.com>
2024-03-14 17:46:04 +0530

    predict

---
+++ PRACTISE QUESTIONS OF C++/13 MARCH/predictoutput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int j;
+    while (j <= 10) 
+    {
+        cout << j << endl;
+        j = j + 1;
+    }
+    return 0;
+}
+
+// NO OUTPUT BECAUSE THE VALUE OF J IS NOT DECLARED OR INITIALISED
