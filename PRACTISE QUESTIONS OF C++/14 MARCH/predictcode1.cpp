3674f2859033e44503cfc751f299762ff47b8bf9
Nancy-Shrown <nancyshrown@gmail.com>
2024-03-14 18:25:28 +0530

    complete

---
+++ PRACTISE QUESTIONS OF C++/14 MARCH/predictcode1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    while ('a' < 'b')
+        cout << "malayalam is a palindrome" << endl;
+    return 0;
+}
+
+/*output
+   continous loop of "malayalam is a palindrome"
+ */
