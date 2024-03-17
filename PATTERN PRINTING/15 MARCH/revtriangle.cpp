46293988f92692a9f92aa499069c0d32e5d996a9
Nancy-Shrown <nancyshrown@gmail.com>
2024-03-17 16:16:29 +0530

    success

---
+++ PATTERN PRINTING/15 MARCH/revtriangle.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+/*print
+         ****
+         ***
+         **
+         *
+*/
+int main()
+{
+    int i,j;
+    for (i = 4; i >= 1; --i) 
+    {
+		for ( j = 1; j <= i; ++j) 
+        {
+			cout << "* ";
+		}
+		cout << endl;
+    }
+    return 0;
+}
