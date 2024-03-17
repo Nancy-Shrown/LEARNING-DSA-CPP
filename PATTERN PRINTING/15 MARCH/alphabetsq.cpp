367bf095c5687b988d2ec6a8248a4213c0d7b936
Nancy-Shrown <nancyshrown@gmail.com>
2024-03-17 16:35:08 +0530

    success

---
+++ PATTERN PRINTING/15 MARCH/alphabetsq.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+/*print
+         ABCD
+         ABCD
+         ABCD
+         ABCD
+*/
+
+int main()
+{
+    int i,j;
+    for(i=0;i<4;i++)
+    {
+        for(j=0;j<1;j++)
+        {
+            cout<<"ABCD";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
