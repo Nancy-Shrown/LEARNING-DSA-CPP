16cacbc749e2a0a529a6b8a86ab0168dc79ddc4b
Nancy-Shrown <nancyshrown@gmail.com>
2024-03-18 21:56:21 +0530

    updated

--- PATTERN PRINTING/15 MARCH/solidsquare.cpp
+++ PATTERN PRINTING/15 MARCH/solidsquare.cpp
@@ -10,10 +10,12 @@ using namespace std;
 
 int main()
 {
-    int i,j;
-    for(i=0;i<4;i++)
+    int i,j,n;
+    cout<<"Enter number :";
+    cin>>n;
+    for(i=0;i<n;i++)
     {
-        for(j=0;j<4;j++)
+        for(j=0;j<n;j++)
         {
             cout<<"*";
         }
