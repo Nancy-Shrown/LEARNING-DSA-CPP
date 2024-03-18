6a62a59efbac6dd4a8a6323cc20cc4e27184b2d2
Nancy-Shrown <nancyshrown@gmail.com>
2024-03-18 22:02:29 +0530

    updated

--- PATTERN PRINTING/15 MARCH/startriangle.cpp
+++ PATTERN PRINTING/15 MARCH/startriangle.cpp
@@ -8,8 +8,10 @@ using namespace std;
 */
 int main()
 {
-    int i,j;
-    for(i=0;i<4;i++)
+    int i,j,n;
+    cout<<"Enter number :";
+    cin>>n;
+    for(i=0;i<n;i++)
     {
         for(j=0;j<=i;j++)
         {
