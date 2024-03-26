5a0fb3574608622c17cceee72c61ce144d2d53e7
Nancy-Shrown <nancyshrown@gmail.com>
2024-03-26 22:01:34 +0530

    success

---
+++ DSA - ARRAYS/sumArr.cpp
@@ -0,0 +1,20 @@
+/*
+Calculate the sum of all the elements in the
+given array.
+*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int array[]= {56,25,26,19,59};
+    int size=sizeof(array)/sizeof(array[0]);
+    int sum=0;
+
+    for(int idx=0;idx<size;idx++)
+    {
+        sum= sum+array[idx];
+    }
+    cout<<"Sum of elements="<<sum;
+    return 0;
+}
