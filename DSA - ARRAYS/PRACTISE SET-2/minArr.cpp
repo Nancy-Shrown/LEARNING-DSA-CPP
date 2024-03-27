b549f24e5a3e0596cb0f89f69ecc975245c41f6a
Nancy-Shrown <nancyshrown@gmail.com>
2024-03-27 20:18:00 +0530

    success

--- DSA - ARRAYS/PRACTISE SET-2/minArr.cpp
+++ DSA - ARRAYS/PRACTISE SET-2/minArr.cpp
@@ -1,4 +1,4 @@
-/*Find the maximum value out of all the elements in the array.*/
+/*Find the minimum value out of all the elements in the array.*/
 #include<iostream>
 using namespace std;
 
@@ -6,15 +6,15 @@ int main()
 {
     int array[]={25,12,99,45,95};
     int size=sizeof(array)/sizeof(array[0]);
-    int max=array[0];
+    int min=array[0];
 
     for(int idx=0;idx<size;idx++)
     {
-        if(array[idx]>max)
+        if(array[idx]<min)
         {
-            max=array[idx];
+            min=array[idx];
         }
     }
-    cout<<"Maximum value = "<<max;
+    cout<<"Minimum value = "<<min;
     return 0;
 }
--- DSA - ARRAYS/PRACTISE SET-2/productArr.cpp
+++ DSA - ARRAYS/PRACTISE SET-2/productArr.cpp
@@ -6,12 +6,12 @@ int main()
 {
     int array[]= {6,4,2,1};
     int size=sizeof(array)/sizeof(array[0]);
-    int product=0;
+    int product=1;
 
     for(int idx=0;idx<size;idx++)
     {
         product= product*array[idx];
     }
-    cout<<"Product of elements="<<sum;
+    cout<<"Product of elements="<<product;
     return 0;
 }
